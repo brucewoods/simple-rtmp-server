27f45a4991991caca93567c3056952dcf274f03c
hzphust <hzphust@yeah.net>
2014-12-19 16:53:27 +0800

    format tab and non-space

--- trunk/src/app/srs_app_tb_log.cpp
+++ trunk/src/app/srs_app_tb_log.cpp
@@ -1,25 +1,25 @@
 /*
-The MIT License (MIT)
+   The MIT License (MIT)
 
-Copyright (c) 2013-2014 winlin
+   Copyright (c) 2013-2014 winlin
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
+   Permission is hereby granted, free of charge, to any person obtaining a copy of
+   this software and associated documentation files (the "Software"), to deal in
+   the Software without restriction, including without limitation the rights to
+   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+   the Software, and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   */
 
 #include <srs_app_tb_log.hpp>
 
@@ -45,14 +45,14 @@ using namespace std;
 int64_t SrsIdAlloc::generate_log_id()
 {
 	timeval tv;
-    if (gettimeofday(&tv, NULL) == -1) {
-        return 0;
-    }
-	
+	if (gettimeofday(&tv, NULL) == -1) {
+		return 0;
+	}
+
 	struct tm* tm;
-    if ((tm = localtime(&tv.tv_sec)) == NULL) {
-        return 0;
-    }
+	if ((tm = localtime(&tv.tv_sec)) == NULL) {
+		return 0;
+	}
 
 	srand((unsigned)time(0));
 	int random_num = rand()  % 1000;
@@ -63,14 +63,14 @@ int64_t SrsIdAlloc::generate_log_id()
 int64_t SrsIdAlloc::generate_conn_id()
 {
 	timeval tv;
-    if (gettimeofday(&tv, NULL) == -1) {
-        return 0;
-    }
-	
+	if (gettimeofday(&tv, NULL) == -1) {
+		return 0;
+	}
+
 	struct tm* tm;
-    if ((tm = localtime(&tv.tv_sec)) == NULL) {
-        return 0;
-    }
+	if ((tm = localtime(&tv.tv_sec)) == NULL) {
+		return 0;
+	}
 
 	srand((unsigned)time(0));
 	int random_num = rand()  % 1000;
@@ -81,37 +81,37 @@ int64_t SrsIdAlloc::generate_conn_id()
 
 SrsTbLog::SrsTbLog()
 {
-    _level = DEFAULT_TB_LOG_LEVEL;
-    log_data = new char[TB_LOG_MAX_SIZE];
+	_level = DEFAULT_TB_LOG_LEVEL;
+	log_data = new char[TB_LOG_MAX_SIZE];
 
-    fd = -1;
+	fd = -1;
 	wf_fd = -1;
-    log_to_file_tank = true;
+	log_to_file_tank = true;
 }
 
 SrsTbLog::~SrsTbLog()
 {
-    srs_freep(log_data);
+	srs_freep(log_data);
 
-    if (fd > 0) {
-        ::close(fd);
-        fd = -1;
-    }
+	if (fd > 0) {
+		::close(fd);
+		fd = -1;
+	}
 
 	if (wf_fd > 0) {
-        ::close(wf_fd);
-        wf_fd = -1;
-    }
+		::close(wf_fd);
+		wf_fd = -1;
+	}
 }
 
 int SrsTbLog::initialize()
 {
-    int ret = ERROR_SUCCESS;
+	int ret = ERROR_SUCCESS;
 
-    log_to_file_tank = true;
-  	_level = 0x03;
-    
-    return ret;
+	log_to_file_tank = true;
+	_level = 0x03;
+
+	return ret;
 }
 
 void SrsTbLog::debug(const char* fmt, ...)
@@ -121,17 +121,17 @@ void SrsTbLog::debug(const char* fmt, ...)
 		return;
 	}
 	int size = 0;
-    if (!generate_header("DEBUG", &size)) {
-        return;
-    }
-    
-    va_list ap;
-    va_start(ap, fmt);
-    // we reserved 1 bytes for the new line.
-    size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
-    va_end(ap);
+	if (!generate_header("DEBUG", &size)) {
+		return;
+	}
 
-    write_log(false, log_data, size, TbLogLevel::Debug);
+	va_list ap;
+	va_start(ap, fmt);
+	// we reserved 1 bytes for the new line.
+	size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
+	va_end(ap);
+
+	write_log(false, log_data, size, TbLogLevel::Debug);
 }
 
 void SrsTbLog::notice(const char* fmt, ...)
@@ -141,88 +141,88 @@ void SrsTbLog::notice(const char* fmt, ...)
 		return;
 	}
 	int size = 0;
-    if (!generate_header("NOTICE", &size)) {
-        return;
-    }
-    
-    va_list ap;
-    va_start(ap, fmt);
-    // we reserved 1 bytes for the new line.
-    size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
-    va_end(ap);
+	if (!generate_header("NOTICE", &size)) {
+		return;
+	}
 
-    write_log(false, log_data, size, TbLogLevel::Notice);
+	va_list ap;
+	va_start(ap, fmt);
+	// we reserved 1 bytes for the new line.
+	size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
+	va_end(ap);
+
+	write_log(false, log_data, size, TbLogLevel::Notice);
 }
 
 void SrsTbLog::warn(const char* fmt, ...)
 {
-    if (_level > TbLogLevel::Warn) {
-        return;
-    }
-    
-    int size = 0;
-    if (!generate_header("WARNING", &size)) {
-        return;
-    }
-    
-    va_list ap;
-    va_start(ap, fmt);
-    // we reserved 1 bytes for the new line.
-    size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
-    va_end(ap);
+	if (_level > TbLogLevel::Warn) {
+		return;
+	}
 
-    write_log(true, log_data, size, TbLogLevel::Warn);
+	int size = 0;
+	if (!generate_header("WARNING", &size)) {
+		return;
+	}
+
+	va_list ap;
+	va_start(ap, fmt);
+	// we reserved 1 bytes for the new line.
+	size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
+	va_end(ap);
+
+	write_log(true, log_data, size, TbLogLevel::Warn);
 }
 
 void SrsTbLog::error(const char* fmt, ...)
 {
-    if (_level > TbLogLevel::Error) {
-        return;
-    }
-    
-    int size = 0;
-    if (!generate_header("ERROR", &size)) {
-        return;
-    }
-    
-    va_list ap;
-    va_start(ap, fmt);
-    // we reserved 1 bytes for the new line.
-    size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
-    va_end(ap);
+	if (_level > TbLogLevel::Error) {
+		return;
+	}
 
-    write_log(true, log_data, size, TbLogLevel::Error);
+	int size = 0;
+	if (!generate_header("ERROR", &size)) {
+		return;
+	}
+
+	va_list ap;
+	va_start(ap, fmt);
+	// we reserved 1 bytes for the new line.
+	size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
+	va_end(ap);
+
+	write_log(true, log_data, size, TbLogLevel::Error);
 }
 
 void SrsTbLog::fatal(const char* fmt, ...)
 {
-    if (_level > TbLogLevel::Fatal) {
-        return;
-    }
-    
-    int size = 0;
-    if (!generate_header("FATAL", &size)) {
-        return;
-    }
-    
-    va_list ap;
-    va_start(ap, fmt);
-    // we reserved 1 bytes for the new line.
-    size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
-    va_end(ap);
+	if (_level > TbLogLevel::Fatal) {
+		return;
+	}
 
-    write_log(true, log_data, size, TbLogLevel::Fatal);
+	int size = 0;
+	if (!generate_header("FATAL", &size)) {
+		return;
+	}
+
+	va_list ap;
+	va_start(ap, fmt);
+	// we reserved 1 bytes for the new line.
+	size += vsnprintf(log_data + size, TB_LOG_MAX_SIZE - size, fmt, ap);
+	va_end(ap);
+
+	write_log(true, log_data, size, TbLogLevel::Fatal);
 }
 
 void SrsTbLog::conn_log(int log_level, string log_type, SrsRequest* req, const char * fmt,...)
 {
 	char msg[TB_LOG_MAX_SIZE] = "";
 	va_list ap;
-    va_start(ap, fmt);
-    // we reserved 1 bytes for the new line.
-    int real_size = vsnprintf(msg, TB_LOG_MAX_SIZE, fmt, ap);
+	va_start(ap, fmt);
+	// we reserved 1 bytes for the new line.
+	int real_size = vsnprintf(msg, TB_LOG_MAX_SIZE, fmt, ap);
 	string str_msg(msg, real_size);
-    va_end(ap);
+	va_end(ap);
 	stringstream ss;
 	ss << "["
 		<< "logid=" << SrsIdAlloc::generate_log_id() << " "
@@ -241,34 +241,34 @@ void SrsTbLog::conn_log(int log_level, string log_type, SrsRequest* req, const c
 	switch(log_level)
 	{
 		case TbLogLevel::Debug:
-		{
-			tb_debug(log_body.c_str());
-			break;
-		}
+			{
+				tb_debug(log_body.c_str());
+				break;
+			}
 		case TbLogLevel::Notice:
-		{
-			tb_notice(log_body.c_str());
-			break;
-		}
+			{
+				tb_notice(log_body.c_str());
+				break;
+			}
 		case TbLogLevel::Warn:
-		{
-			tb_warn(log_body.c_str());
-			break;
-		}
+			{
+				tb_warn(log_body.c_str());
+				break;
+			}
 		case TbLogLevel::Error:
-		{
-			tb_error(log_body.c_str());
-			break;
-		}
+			{
+				tb_error(log_body.c_str());
+				break;
+			}
 		case TbLogLevel::Fatal:
-		{
-			tb_fatal(log_body.c_str());
-			break;
-		}
+			{
+				tb_fatal(log_body.c_str());
+				break;
+			}
 		default:
-		{
-			tb_warn("wrong log type!");
-		}
+			{
+				tb_warn("wrong log type!");
+			}
 	}
 	return;	
 }
@@ -277,11 +277,11 @@ void SrsTbLog::global_log(int log_level,const char * fmt,...)
 {
 	char msg[TB_LOG_MAX_SIZE] = "";
 	va_list ap;
-    va_start(ap, fmt);
-    // we reserved 1 bytes for the new line.
-    int real_size = vsnprintf(msg, TB_LOG_MAX_SIZE, fmt, ap);
+	va_start(ap, fmt);
+	// we reserved 1 bytes for the new line.
+	int real_size = vsnprintf(msg, TB_LOG_MAX_SIZE, fmt, ap);
 	string str_msg(msg, real_size);
-    va_end(ap);
+	va_end(ap);
 	stringstream ss;
 	ss << "["
 		<< "logid=" << SrsIdAlloc::generate_log_id() << " "
@@ -293,79 +293,79 @@ void SrsTbLog::global_log(int log_level,const char * fmt,...)
 	switch(log_level)
 	{
 		case TbLogLevel::Debug:
-		{
-			tb_debug(log_body.c_str());
-			break;
-		}
+			{
+				tb_debug(log_body.c_str());
+				break;
+			}
 		case TbLogLevel::Notice:
-		{
-			tb_notice(log_body.c_str());
-			break;
-		}
+			{
+				tb_notice(log_body.c_str());
+				break;
+			}
 		case TbLogLevel::Warn:
-		{
-			tb_warn(log_body.c_str());
-			break;
-		}
+			{
+				tb_warn(log_body.c_str());
+				break;
+			}
 		case TbLogLevel::Error:
-		{
-			tb_error(log_body.c_str());
-			break;
-		}
+			{
+				tb_error(log_body.c_str());
+				break;
+			}
 		case TbLogLevel::Fatal:
-		{
-			tb_fatal(log_body.c_str());
-			break;
-		}
+			{
+				tb_fatal(log_body.c_str());
+				break;
+			}
 		default:
-		{
-			tb_warn("wrong log type!");
-		}
+			{
+				tb_warn("wrong log type!");
+			}
 	}
 	return;	
 }
 
 bool SrsTbLog::generate_header(const char* level_name, int* header_size)
 {
-    // clock time
-    timeval tv;
-    if (gettimeofday(&tv, NULL) == -1) {
-        return false;
-    }
-    
-    // to calendar time
-    struct tm* tm;
-    if ((tm = localtime(&tv.tv_sec)) == NULL) {
-        return false;
-    }
-    
-    // write log header
-    int log_header_size = -1;
-    
-    log_header_size = snprintf(log_data, TB_LOG_MAX_SIZE, 
-         "%s: %d-%02d-%02d %02d:%02d:%02d TB_LIVE ", 
-          level_name, 1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+	// clock time
+	timeval tv;
+	if (gettimeofday(&tv, NULL) == -1) {
+		return false;
+	}
 
-    if (log_header_size == -1) {
-        return false;
-    }
-    
-    // write the header size.
-    *header_size = srs_min(TB_LOG_MAX_SIZE - 1, log_header_size);
-    
-    return true;
+	// to calendar time
+	struct tm* tm;
+	if ((tm = localtime(&tv.tv_sec)) == NULL) {
+		return false;
+	}
+
+	// write log header
+	int log_header_size = -1;
+
+	log_header_size = snprintf(log_data, TB_LOG_MAX_SIZE, 
+			"%s: %d-%02d-%02d %02d:%02d:%02d TB_LIVE ", 
+			level_name, 1900 + tm->tm_year, 1 + tm->tm_mon, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+
+	if (log_header_size == -1) {
+		return false;
+	}
+
+	// write the header size.
+	*header_size = srs_min(TB_LOG_MAX_SIZE - 1, log_header_size);
+
+	return true;
 }
 
 void SrsTbLog::write_log(bool is_except, char *str_log, int size, int level)
 {
-    // ensure the tail and EOF of string
-    //      LOG_TAIL_SIZE for the TAIL char.
-    //      1 for the last char(0).
-    size = srs_min(TB_LOG_MAX_SIZE - 1 - TB_LOG_TAIL_SIZE, size);
-    
-    // add some to the end of char.
-    str_log[size++] = TB_LOG_TAIL;
-    str_log[size++] = 0;
+	// ensure the tail and EOF of string
+	//      LOG_TAIL_SIZE for the TAIL char.
+	//      1 for the last char(0).
+	size = srs_min(TB_LOG_MAX_SIZE - 1 - TB_LOG_TAIL_SIZE, size);
+
+	// add some to the end of char.
+	str_log[size++] = TB_LOG_TAIL;
+	str_log[size++] = 0;
 
 	if (!is_except)		//notice
 	{
@@ -395,38 +395,38 @@ void SrsTbLog::write_log(bool is_except, char *str_log, int size, int level)
 
 void SrsTbLog::open_log_file()
 {
-    std::string filename = TB_LOG_FILE;
-    
-    if (filename.empty()) {
-        return;
-    }
-    
-    fd = ::open(filename.c_str(), O_RDWR | O_APPEND);
-    
-    if(fd == -1 && errno == ENOENT) {
-        fd = open(filename.c_str(), 
-            O_RDWR | O_CREAT | O_TRUNC, 
-            S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
-        );
-    }
+	std::string filename = TB_LOG_FILE;
+
+	if (filename.empty()) {
+		return;
+	}
+
+	fd = ::open(filename.c_str(), O_RDWR | O_APPEND);
+
+	if(fd == -1 && errno == ENOENT) {
+		fd = open(filename.c_str(), 
+				O_RDWR | O_CREAT | O_TRUNC, 
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
+				);
+	}
 }
 
 void SrsTbLog::open_wf_log_file()
 {
-    std::string filename = TB_WF_LOG_FILE;
-    
-    if (filename.empty()) {
-        return;
-    }
-    
-    wf_fd = ::open(filename.c_str(), O_RDWR | O_APPEND);
-    
-    if(wf_fd == -1 && errno == ENOENT) {
-        wf_fd = open(filename.c_str(), 
-            O_RDWR | O_CREAT | O_TRUNC, 
-            S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
-        );
-    }
+	std::string filename = TB_WF_LOG_FILE;
+
+	if (filename.empty()) {
+		return;
+	}
+
+	wf_fd = ::open(filename.c_str(), O_RDWR | O_APPEND);
+
+	if(wf_fd == -1 && errno == ENOENT) {
+		wf_fd = open(filename.c_str(), 
+				O_RDWR | O_CREAT | O_TRUNC, 
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
+				);
+	}
 }
 
 
--- trunk/src/app/srs_app_tb_log.hpp
+++ trunk/src/app/srs_app_tb_log.hpp
@@ -1,25 +1,25 @@
 /*
-The MIT License (MIT)
+   The MIT License (MIT)
 
-Copyright (c) 2013-2014 winlin
+   Copyright (c) 2013-2014 winlin
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
+   Permission is hereby granted, free of charge, to any person obtaining a copy of
+   this software and associated documentation files (the "Software"), to deal in
+   the Software without restriction, including without limitation the rights to
+   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+   the Software, and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   */
 
 #ifndef SRS_APP_TB_LOG_HPP
 #define SRS_APP_TB_LOG_HPP
@@ -59,51 +59,51 @@ const std::string LOGTYPE_GLOBAL_STAT = "global_stat";
 
 class SrsIdAlloc
 {
-private:
-public:
-	static int64_t generate_log_id();
-	static int64_t generate_conn_id();
+	private:
+	public:
+		static int64_t generate_log_id();
+		static int64_t generate_conn_id();
 };
 
 /**
-* we use memory/disk cache and donot flush when write log.
-* it's ok to use it without config, which will log to console, and default trace level.
-* when you want to use different level, override this classs, set the protected _level.
-*/
+ * we use memory/disk cache and donot flush when write log.
+ * it's ok to use it without config, which will log to console, and default trace level.
+ * when you want to use different level, override this classs, set the protected _level.
+ */
 
 class SrsRequest;
 
 class SrsTbLog : public ITbLog
 {
-// for utest to override
-protected:
-    // defined in SrsLogLevel.
-    int _level;
-private:
-    char* log_data;
-    // log to file if specified srs_log_file
-    int fd;
-	// exception log
-	int wf_fd;
-    // whether log to file tank
-    bool log_to_file_tank;
-public:
-    SrsTbLog();
-    virtual ~SrsTbLog();
-public:
-    virtual int initialize();
-	virtual void debug(const char* fmt, ...);
-    virtual void notice(const char* fmt, ...);
-    virtual void warn(const char* fmt, ...);
-    virtual void error(const char* fmt, ...);
-    virtual void fatal(const char* fmt, ...);
-	virtual void conn_log(int log_level, std::string log_type, SrsRequest* req, const char* fmt, ...);
-	virtual void global_log(int log_level, const char* fmt, ...);
-private:
-    virtual bool generate_header(const char* level_name, int* header_size);
-    virtual void write_log(bool is_except, char* str_log, int size, int level);
-    virtual void open_log_file();
-    virtual void open_wf_log_file();
+	// for utest to override
+	protected:
+		// defined in SrsLogLevel.
+		int _level;
+	private:
+		char* log_data;
+		// log to file if specified srs_log_file
+		int fd;
+		// exception log
+		int wf_fd;
+		// whether log to file tank
+		bool log_to_file_tank;
+	public:
+		SrsTbLog();
+		virtual ~SrsTbLog();
+	public:
+		virtual int initialize();
+		virtual void debug(const char* fmt, ...);
+		virtual void notice(const char* fmt, ...);
+		virtual void warn(const char* fmt, ...);
+		virtual void error(const char* fmt, ...);
+		virtual void fatal(const char* fmt, ...);
+		virtual void conn_log(int log_level, std::string log_type, SrsRequest* req, const char* fmt, ...);
+		virtual void global_log(int log_level, const char* fmt, ...);
+	private:
+		virtual bool generate_header(const char* level_name, int* header_size);
+		virtual void write_log(bool is_except, char* str_log, int size, int level);
+		virtual void open_log_file();
+		virtual void open_wf_log_file();
 };
 
 #endif
